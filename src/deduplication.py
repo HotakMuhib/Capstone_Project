8a2bf0ba9cb38d810b7be6d087ca80d26ec292f0
JasonChay <chayjason85@gmail.com>
2026-02-13 09:43:10 -0500

    Split data set

--- src/deduplication.py
+++ src/deduplication.py
@@ -10,12 +10,12 @@ def deduplicate(df):
     
     # First drop duplicates based on Transaction ID
     deduped_id = df[~df.duplicated(subset=['Transaction ID'], keep='first')] # get df excluding duplicates
-    rejected_id = df[df.duplicated(subset=['Transaction ID'], keep='first')] # get actual duplicates
+    rejected_id = df[df.duplicated(subset=['Transaction ID'], keep='first')].copy() # get actual duplicates
     rejected_id['Error Info'] = 'Duplicate primary key' # adding an error column
 
     # Then drop duplicates based on unique_identifiers
     deduped_all = deduped_id[~deduped_id.duplicated(subset=unique_identifiers, keep='first')]
-    rejected_unique = deduped_id[deduped_id.duplicated(subset=unique_identifiers, keep='first')] # get rows that were removed again
+    rejected_unique = deduped_id[deduped_id.duplicated(subset=unique_identifiers, keep='first')].copy() # get rows that were removed again
     rejected_unique['Error Info'] = 'Duplicate subset of unique identifiers'
 
     # the combination of both rejected records based on Transaction ID and the unique_identifiers
